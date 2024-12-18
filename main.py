05f5e7ef32ae5dacaa29388d13201c28db66ccb3
saturnaliam <luciacdev@gmail.com>
2024-12-17 22:49:15 -0600

    added performance timing, cleaned up code, proper exporting to csv

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
+import sys
+import time
 
 load_dotenv()
 scope = "user-library-read"
@@ -15,20 +19,53 @@ def format(ms):
     result = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
     return result
 
-def print_info():
+albums = []
+def get_album_info():
+    print("fetching information from spotify...")
+    global albums
+
+    initial_time = time.perf_counter()
     results = sp.current_user_saved_albums(limit=50)
-    
     albums = results['items']
     while results['next']:
         results = sp.next(results)
         albums.extend(results['items'])
 
+    print(f"fetching finished in {(time.perf_counter() - initial_time):0.2f} seconds")
+
+def print_info():
     for album in albums:
         album = album['album']
 
         duration = 0
         for track in album['tracks']['items']:
             duration += track['duration_ms']
-        print(album['name'], "-", album['artists'][0]['name'], " ", format(duration))
+        print(album['name'], "-", album['artists'][0]['name'], format(duration))
 
-print_info()
+def output_csv(filename):
+    formatted = '"Album","Artist","Duration","Total Length",\n'
+
+    formula_added = False
+    for album in albums:
+        album = album['album']
+        duration = 0
+        for track in album['tracks']['items']:
+            duration += track['duration_ms']
+        
+        formatted += f'"{album['name']}","{album['artists'][0]['name']}","{format(duration)}",'
+
+        if not formula_added:
+            formatted += f'"=SUM(C2:C{len(albums) + 1})"'
+            formula_added = True
+
+        formatted += '\n'
+
+    with open(filename, 'w') as file:
+        file.write(formatted)
+
+get_album_info()
+
+if len(sys.argv) == 2:
+    output_csv(sys.argv[1])
+else:
+    print_info()
